f30b9d34440f619ed23b3afa8354bf68e70eeff8
malithie <malithim@wso2.com>
2023-12-03 13:17:18 +0530

    Merge b2c app code from https://github.com/malithie/choreo-examples/

---
+++ b2c/petdesk/web/react/config.js
@@ -0,0 +1,8 @@
+window.config = {
+    baseUrl: "",
+    clientID: "",
+    signInRedirectURL: "http://127.0.0.1:5173/",
+    signOutRedirectURL: "http://127.0.0.1:5173/",
+    resourceServerURL: "",
+    scope: ["openid", "email", "profile"]
+ };
